dca4752169caec958facbb703b6d626f01b0a5a5
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-05-18 22:09:50 +0700

    add page for create user

--- domains/users/routes.js
+++ domains/users/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const { User } = require('../../database/models');
+const { UserBio } = require('../../database/models');
 
 const userRouter = express.Router();
 
-// POST: /api/v1/users/login
+// POST: /form-dashboard/users/login
 // endpoint yang bertugas menerima data login form dari halaman dashboard/login
 userRouter.post('/form-dashboard/users/login', async function(req, res) {
     const username = req.body.username;
@@ -14,6 +15,7 @@ userRouter.post('/form-dashboard/users/login', async function(req, res) {
         where: {
             username: username,
             password: password,
+            role: 'ADMIN',
         },
     });
 
@@ -22,6 +24,33 @@ userRouter.post('/form-dashboard/users/login', async function(req, res) {
 
         return;
     }
+
+    res.redirect('/dashboard/home');
+});
+
+// POST: /form-dashboard/users/create
+// endpoint yang bertugas menerima data new user dari halaman dashboard create user
+userRouter.post('/form-dashboard/users/create', async function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const address = req.body.address;
+    const hobby = req.body.hobby;
+
+    const created = await User.create({
+        username,
+        password,
+        role: 'PLAYER',
+    });
+
+    await UserBio.create({
+        user_id: created.id,
+        first_name: firstName,
+        last_name: lastName,
+        address,
+        hobby,
+    });
 
     res.redirect('/dashboard/home');
 });
