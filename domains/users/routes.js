3b733fd2e41a4539e8bb3b4cc109ccd4694b99d0
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-05-18 21:11:49 +0700

    add page login

--- domains/users/routes.js
+++ domains/users/routes.js
@@ -4,6 +4,26 @@ const { User } = require('../../database/models');
 
 const userRouter = express.Router();
 
-// userRouter.get();
+// POST: /api/v1/users/login
+// endpoint yang bertugas menerima data login form dari halaman dashboard/login
+userRouter.post('/api/v1/users/login', async function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await User.findOne({
+        where: {
+            username: username,
+            password: password,
+        },
+    });
+
+    if (!user) {
+        res.redirect('/dashboard/login');
+
+        return;
+    }
+
+    res.redirect('/dashboard/home');
+});
 
 module.exports = userRouter;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 const gameRouter = require('./domains/games/routes');
 const dashboardRouter = require('./domains/dashboard/routes');
+const userRouter = require('./domains/users/routes');
 
 const PORT = process.env.PORT;
 
@@ -13,10 +14,12 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static('public/static'));
 
 app.use(gameRouter);
 app.use(dashboardRouter);
+app.use(userRouter);
 
 app.listen(PORT, function() {
     console.log(`server listening on port: ${PORT}`);
