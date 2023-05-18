2c0bb67a33a35d24a0810ed9383a7bb3e473b3b4
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-05-18 20:44:59 +0700

    add dashboard with views

--- server.js
+++ server.js
@@ -8,9 +8,41 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(express.json());
+app.use('/static', express.static('public/static'));
 
 app.use(gameRouter);
+
+app.get('/', function(req, res) {
+    res.redirect('/dashboard/login');
+})
+
+app.get('/dashboard/login', function(req, res) {
+    res.render('dashboard/login');
+});
+
+app.get('/dashboard/home', function(req, res) {
+    res.render('dashboard/home');
+});
+
+app.get('/dashboard/users/create', function(req, res) {
+    res.render('dashboard/users/create');
+});
+
+app.get('/dashboard/users/:id/update', function(req, res) {
+    const id = req.params.id;
+
+    res.render('dashboard/users/update');
+});
+
+app.get('/dashboard/users/:id/delete', function(req, res) {
+    const id = req.params.id;
+
+    res.render('dashboard/users/delete');
+});
 
 app.listen(PORT, function() {
     console.log(`server listening on port: ${PORT}`);
