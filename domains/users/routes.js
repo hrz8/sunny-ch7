a70cc14d4c36b4fbf25fd3099113da694c6e8e63
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2023-05-20 10:01:34 +0700

    add restful for CRUD user

--- domains/users/routes.js
+++ domains/users/routes.js
@@ -91,22 +91,193 @@ userRouter.post('/form-dashboard/users/update', async function(req, res) {
 userRouter.post('/form-dashboard/users/delete', async function(req, res) {
     const id = req.body.user_id;
 
-    try {
-        await UserBio.destroy({
-            where: {
-                user_id: id,
-            },
-        });
-        await User.destroy({
-            where: {
-                id,
-            },
-        });
-    } catch (error) {
-        console.error(error);
-    }
+    await UserBio.destroy({
+        where: {
+            user_id: id,
+        },
+    });
+    await User.destroy({
+        where: {
+            id,
+        },
+    });
 
     res.redirect('/dashboard/home');
+});
+
+// GET: /api/v1/users -> list user
+userRouter.get('/api/v1/users', async function(req, res) {
+    const users = await User.findAll({
+        include: {
+            model: UserBio,
+            as: 'bio',
+        },
+        order: [
+            ['id', 'ASC'],
+        ],
+    });
+
+    res.json({
+        message: 'success fetch user data',
+        result: users.map(function(user) {
+            return {
+                id: user.id,
+                username: user.username,
+                role: user.role,
+                full_name: `${user.bio.first_name} ${user.bio.last_name}`,
+                hobby: user.bio.hobby,
+                address: user.bio.address,
+            }
+        }),
+        error: null,
+    });
+});
+
+// GET: /api/v1/users/:id -> detail user
+userRouter.get('/api/v1/users/:id', async function(req, res) {
+    const id = req.params.id;
+
+    const currentUser = await User.findOne({
+        where: {
+            id,
+        },
+        include: {
+            model: UserBio,
+            as: 'bio',
+        },
+    });
+
+    if (!currentUser) {
+        res.status(404);
+        res.json({
+            message: 'error when get user detail',
+            result: null,
+            error: 'user with that id is not found'
+        });
+
+        return;
+    }
+
+    res.json({
+        message: 'success get user detail',
+        result: {
+            id: currentUser.id,
+            username: currentUser.username,
+            role: currentUser.role,
+            full_name: `${currentUser.bio.first_name} ${currentUser.bio.last_name}`,
+            hobby: currentUser.bio.hobby,
+            address: currentUser.bio.address,
+        },
+        error: null,
+    });
+});
+
+// POST: /api/v1/users -> create user
+userRouter.post('/api/v1/users', async function(req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const address = req.body.address;
+    const hobby = req.body.hobby;
+
+    const created = await User.create({
+        username,
+        password,
+        role: 'PLAYER',
+    });
+
+    await UserBio.create({
+        user_id: created.id,
+        first_name: firstName,
+        last_name: lastName,
+        address,
+        hobby,
+    });
+
+    res.json({
+        message: 'success create new user',
+        result: created,
+        error: null,
+    });
+});
+
+// PUT: /api/v1/users/:id -> update user
+userRouter.put('/api/v1/users/:id', async function(req, res) {
+    const id = req.params.id;
+    const username = req.body.username;
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const address = req.body.address;
+    const hobby = req.body.hobby;
+
+    const updated = await UserBio.update({
+        first_name: firstName,
+        last_name: lastName,
+        address,
+        hobby,
+    }, {
+        where: {
+            user_id: id,
+        },
+    });
+
+    res.json({
+        message: 'success update user',
+        result: updated,
+        error: null,
+    });
+});
+
+// DELETE: /api/v1/users/:id -> delete user
+userRouter.delete('/api/v1/users/:id', async function(req, res) {
+    const id = req.params.id;
+
+    const currentUser = await User.findOne({
+        where: {
+            id: id,
+        }
+    });
+
+    if (!currentUser) {
+        res.status(404);
+        res.json({
+            message: 'error when delete user',
+            result: null,
+            error: 'user with that id is not found'
+        });
+
+        return;
+    }
+
+    if (currentUser.role !== 'PLAYER') {
+        res.status(400);
+        res.json({
+            message: 'error when delete',
+            result: null,
+            error: 'only user with role PLAYER can be deleted'
+        });
+
+        return;
+    }
+
+    await UserBio.destroy({
+        where: {
+            user_id: id
+        },
+    });
+
+    await User.destroy({
+        where: {
+            id,
+        },
+    });
+
+    res.json({
+        message: 'success delete user',
+        result: 1,
+        error: null,
+    });
 });
 
 module.exports = userRouter;
